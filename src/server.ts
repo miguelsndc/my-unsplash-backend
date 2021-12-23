84c595c168b96aded3984e90c0212c961a880125
miguelsndc <mglsndc@gmail.com>
2021-12-23 13:45:15 -0300

    feat: server setup

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,23 @@
-require('dotenv').config();
 import express from 'express';
+import 'express-async-errors';
+
+import cors from 'cors';
+import helmet from 'helmet';
+import { config } from 'dotenv';
+import { router } from './routes';
+
+config();
 
 const app = express();
+const PORT = process.env.PORT || 3333;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(router);
+
+app.get('/', (_, res) => res.send('Running'));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at port ${PORT}`);
+});
